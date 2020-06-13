0bf95742c6d280a54c1ad42d6bfe23652c578034
shahid Mehmood <shahid.mehmood.6443@slashnext.com>
2020-06-13 02:55:55 +0200

    updated code

--- config.py
+++ config.py
@@ -1,14 +1,25 @@
-RECIPIENTS = ["shahid.ibex@yahoo.com", "enterprise.notification@canamwireless.com"]
+RECIPIENTS = ["enterprise.notification@canamwireless.com"]
+# RECIPIENTS = ["shahid.ibex@yahoo.com"]
 
 
 # Host configurations
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST = 'smtp.office365.com'
-EMAIL_HOST_PASSWORD = 'Dyvi2030!'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_PASSWORD = 'johan@123'
 EMAIL_HOST_USER = 'enterprise.notification@canamwireless.com'
+
+# EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_HOST_PASSWORD = '1234great5678'
+# EMAIL_HOST_USER = 'mariegreat123@gmail.com'
 
 
 MACHINE_IP = "24.55.9.146"
 
-IMAGE_NAME = "imageToSave.jpeg"
+IMAGE_NAME = "employee_face.jpeg"
+
+TEMPERATURE_LIMIT = 90.0
+
+
+HEADERS = {'Content-Type': 'application/json'}
+BASE_URL = "http://" + MACHINE_IP + ":8080/api/v1/face/"
--- get_faces.py
+++ get_faces.py
@@ -9,31 +9,54 @@ now = datetime.datetime.utcnow()
 now_start = now - timedelta(seconds=2000000000)
 start_time = datetime.datetime.strftime(now_start, '%Y-%m-%d %H:%M:%S')
 end_time = datetime.datetime.strftime(now, '%Y-%m-%d %H:%M:%S')
-headers = {'Content-Type': 'application/json'}
-base_url = "http://" + MACHINE_IP + ":8080/api/v1/face/"
+
+
+def ipc_get_attend_record_count():
+    """
+    Get Total Record Count
+    :return:
+    """
+    payload = {"startId": -1}
+    url = BASE_URL + "getAttendRecordCount"
+
+    response = requests.post(url, json=payload, headers=HEADERS)
+    response = json.loads(response.content.decode('utf-8'))
+    total_records = int(response["totalCount"])
+    return total_records
 
 
 def ipc_query_attend_record():
-    payload = {"startId": -1, "reqCount": 1, "needImg": True}
-    url = base_url + "queryAttendRecord"
-    response = requests.post(url, json=payload, headers=headers)
+    """
+    Get Attendance Records from AI Device
+    :return:
+    """
+    payload = {
+        "startId": -1,
+        "reqCount": 30,
+        "needImg": True
+    }
+    url = BASE_URL + "queryAttendRecord"
+    response = requests.post(url, json=payload, headers=HEADERS, timeout=10)
     response = json.loads(response.content.decode('utf-8'))
 
     data = response['data']
-    for obj in data:
-        # user_id = obj['userid']
-        name = obj['name']
-        # gender = obj['gender']
-        # phone = obj['phone']
-        # id = obj['id']
-        timestamp = obj['timestamp']
-        temperature = float(obj['bodyTemperature'])
-        image = obj['img']
-        print(image)
-        # if temperature < 90.0:
-        send_email(name, temperature, image, timestamp)
-    return response
+    if data:
+        for obj in data:
+            name = obj['name']
+            record_id = obj['id']
+            timestamp = obj['timestamp']
+            temperature_fahrenheit = float(obj['bodyTemperature'])
+            temperature = round((temperature_fahrenheit * 9/5) + 32,2)
+            print("ID:  {}  Name:  {}, Temperature:  {}, TimeStamp:  {}".format(record_id, name, temperature, timestamp))
+            image = obj['img']
+            if temperature > TEMPERATURE_LIMIT:
+                send_email(name, temperature, image, timestamp)
+        return response
+    else:
+        print("NO DATA FOUND")
 
 
 if __name__ == "__main__":
+    record_count = ipc_get_attend_record_count()
+    print("Total Records: ", record_count)
     ipc_query_attend_record()
